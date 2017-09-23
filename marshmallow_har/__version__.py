a96e45e9d8b421ddc704d6ae10b3d81831ad3b8f
Louis-Philippe Huberdeau <lp@huberdeau.info>
2017-09-23 10:55:19 -0400

    Clean-up dependencies, copyright notices

--- marshmallow_har/__version__.py
+++ marshmallow_har/__version__.py
@@ -20,25 +20,4 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-from .model import Cookie, Request, Response, Entry, HAR, Header, Param, PostData, PostParam
-from .model import Log, Page, Creator, Browser, Cache, CacheState, Timings, Content, PageTimings
-
-
-CookieSchema = Cookie.__schema__
-HeaderSchema = Header.__schema__
-ParamSchema = Param.__schema__
-PostParamSchema = PostParam.__schema__
-PostDataSchema = PostData.__schema__
-RequestSchema = Request.__schema__
-ContentSchema = Content.__schema__
-ResponseSchema = Response.__schema__
-CacheStateSchema = CacheState.__schema__
-CacheSchema = Cache.__schema__
-TimingsSchema = Timings.__schema__
-EntrySchema = Entry.__schema__
-CreatorSchema = Creator.__schema__
-BrowserSchema = Browser.__schema__
-PageTimingsSchema = PageTimings.__schema__
-PageSchema = Page.__schema__
-LogSchema = Log.__schema__
-HARSchema = HAR.__schema__
+__version__ = "1.0.0"
--- setup.py
+++ setup.py
@@ -1,14 +1,33 @@
 #!/usr/bin/env python
 
+import os
 from setuptools import setup
 
+version_file = "marshmallow_har/__version__.py"
+version_data = {}
+with open(version_file) as f:
+    code = compile(f.read(), version_file, 'exec')
+    exec(code, globals(), version_data)
+
+
+def dep_list(filename):
+    out = []
+
+    path = os.path.dirname(os.path.abspath(__file__))
+    with open(os.path.join(path, filename)) as fp:
+        for line in fp:
+            clean = line.strip()
+            if clean:
+                out.append(clean)
+
+    return out
+
+
 setup(name='marshmallow-har',
-      version='1.0.0',
+      version=version_data['__version__'],
       description='Simple set of marshmallow schemas to load/dump the HTTP Archive (HAR) format.',
       author='Delve Labs inc.',
       author_email='info@delvelabs.ca',
       url='https://github.com/delvelabs/marshmallow-har',
       packages=['marshmallow_har'],
-      install_requires=[
-          'marshmallow-autoschema',
-      ])
+      install_requires=dep_list("requirements.txt"))
